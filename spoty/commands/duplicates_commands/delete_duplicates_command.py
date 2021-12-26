e54154f4552af73e72973a23bb32e14aed2b05ae
Derwish <derwish.pro@gmail.com>
2021-12-27 01:47:19 +0300

    -

---
+++ spoty/commands/duplicates_commands/delete_duplicates_command.py
@@ -0,0 +1,44 @@
+from spoty import settings
+from spoty import log
+from spoty.utils import SpotyContext
+import spoty.utils
+import click
+
+
+@click.command("print")
+@click.option('--print-pattern', '--pp', show_default=True,
+              default=settings.SPOTY.DEFAULT_PRINT_DUPLICATES_PATTERN,
+              help='Print a list of tracks according to this formatting pattern.')
+@click.pass_obj
+def print_duplicates(context: SpotyContext,
+                     print_pattern,
+                     ):
+    """
+Print a list of duplicates to console.
+    """
+
+    for i, group in enumerate(context.duplicates_groups):
+        click.echo()
+        click.echo(f"--------------------------- GROUP {i+1}/{len(context.duplicates_groups)} ---------------------------")
+        if len(group.source_def_duplicates) > 0:
+            if len(group.source_def_duplicates) == 1:
+                click.echo("Source:")
+            else:
+                click.echo("Source definitely duplicates:")
+            spoty.utils.print_tags_list(group.source_def_duplicates, print_pattern)
+
+        if len(group.source_prob_duplicates) > 0:
+            click.echo("Source probably duplicates:")
+            spoty.utils.print_tags_list(group.source_prob_duplicates, print_pattern)
+
+        if len(group.dest_def_duplicates) > 0:
+            click.echo("Destination definitely duplicates:")
+            spoty.utils.print_tags_list(group.dest_def_duplicates, print_pattern)
+
+        if len(group.dest_prob_duplicates) > 0:
+            click.echo("Destination probably duplicates:")
+            spoty.utils.print_tags_list(group.dest_prob_duplicates, print_pattern)
+
+
+    click.echo('\n------------------------------------------------------------')
+    click.echo('\n'.join(context.summary))
--- spoty/commands/duplicates_commands/export_duplicates_command.py
+++ spoty/commands/duplicates_commands/export_duplicates_command.py
@@ -31,17 +31,28 @@ Export a list of duplicates to csv file.
     date_time_str = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
     file_name = 'duplicates-' + date_time_str
 
+    duplicates_count=0
+
     all_tags_list = []
     for group in context.duplicates_groups:
         if not no_source:
             all_tags_list.extend(group.source_def_duplicates)
             all_tags_list.extend(group.source_prob_duplicates)
+            duplicates_count+=len(group.source_def_duplicates)-1
+            duplicates_count += len(group.source_prob_duplicates)
         all_tags_list.extend(group.dest_def_duplicates)
         all_tags_list.extend(group.dest_prob_duplicates)
+        duplicates_count += len(group.dest_def_duplicates)
+        duplicates_count += len(group.dest_prob_duplicates)
         if not no_split_groups:
             all_tags_list.append({})
 
     spoty.csv_playlist.create_csvs(all_tags_list, path, file_name)
 
+    context.summary.append("Exporting:")
+    if len(all_tags_list) > 0:
+        context.summary.append(f'  {duplicates_count} duplicates exported to csv file ("{os.path.join(path,file_name+".csv")}").')
+    else:
+        context.summary.append(f'  No tracks to export.')
     click.echo('\n------------------------------------------------------------')
     click.echo('\n'.join(context.summary))
