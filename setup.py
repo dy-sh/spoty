16692713ce798d7b2deded4a2ffccc72a9debf8b
Derwish <derwish.pro@gmail.com>
2022-01-09 15:41:01 +0300

    setup

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 from setuptools import setup, find_packages
 
+
 def read_requirements():
     with open('requirements.txt') as file:
         content = file.read()
         requirements = content.split('\n')
-
     return requirements
+
 
 setup(
     name="spoty",
-    version="1.0",
+    version="0.1.0",
+    description="CLI tool for management of Spotify, Deezer and other music services as well as local music files.",
+    author="Dmitry Savosh",
+    author_email="d.savosh@gmail.com",
+    url="https://github.com/dy-sh/spoty",
     packages=find_packages(),
     include_package_data=True,
     install_requires=read_requirements(),
